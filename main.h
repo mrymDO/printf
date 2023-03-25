96b95701ee8a983f5ddfeadb03ab1bfad944c3e3
Mrym <dobli.meryeme@gmail.com>
2023-03-25 04:15:52 -0700

    update files

---
+++ main.h
@@ -0,0 +1,13 @@
+#ifndef "MAIN_H"
+#define "MAIN_H"
+
+
+#include <stdlib.h>
+#include <stdarg.h>
+#include <unistd.h>
+
+int _printf(const char *format, ...);
+int _putchar(char c);
+int _puts(char *s);
+
+#endif
