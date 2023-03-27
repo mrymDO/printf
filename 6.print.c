115a9f95d1368e898887a4dead53e9c28c6b5c9a
Mrym <dobli.meryeme@gmail.com>
2023-03-27 13:19:43 -0700

    6.print pointer fix

--- 6.print.c
+++ 6.print.c
@@ -1,32 +1,35 @@
 #include "main.h"
 
+#define HEX_BASE 16
+#define HEX_LENGTH 16
+#define NULL_STRING "(NULL)"
+#define NULL_STRING_LENGTH 6
+
+
 /**
  * print_pointer - print pointer
- * @ptr: pointer
+ * @ptr : pointer
  * @len: length
  * Return: void
  */
-
 void print_pointer(void *ptr, int *len)
 {
 	unsigned long int addr = (unsigned long int)ptr;
-	char hex[16] = {0};
+	char hex[HEX_LENGTH] = {0};
 	int i, digit;
-
-	_putstr("0x");
 
 	if (addr == 0)
 	{
-		_putstr("(NULL)");
-		*len += 6;
-
+		_putstr(NULL_STRING);
+		*len += NULL_STRING_LENGTH;
 		return;
 	}
 
+	_putstr("0x");
+
 	for (i = 0; addr > 0; i++)
 	{
-		digit = addr % 16;
-
+		digit = addr % HEX_BASE;
 		if (digit < 10)
 		{
 			hex[i] = digit + '0';
@@ -35,13 +38,12 @@ void print_pointer(void *ptr, int *len)
 		{
 			hex[i] = digit - 10 + 'a';
 		}
-
-		addr /= 16;
+		addr /= HEX_BASE;
 	}
+
 	for (i -= 1; i >= 0; i--)
 	{
 		_putchar(hex[i]);
 		*len += 1;
 	}
 }
-
